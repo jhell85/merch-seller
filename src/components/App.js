406d938a4d3eea6e0eecac80bfdd55b1a36b85fb
jhell85 <jhell85@gmail.com>
2020-07-02 14:16:50 -0700

    add README.MD and initial project set up.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+   <React.Fragment>
+     <h1>Hello World</h1>
+   </React.Fragment>
+  );
+}
+
+export default App;
